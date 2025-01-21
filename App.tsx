280578dd3c9da4f449bc58e12b7ea2d56325963d
Henil Gandhi <henil.gandhi2003@gmail.com>
2025-01-21 16:23:30 +0530

    inbox,push,inapp

--- App.tsx
+++ App.tsx
@@ -1,59 +1,266 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// import CleverTap from 'clevertap-react-native';
+// import React, { useEffect } from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// type SectionProps = {
+//   title: string;
+//   children: React.ReactNode;
+// };
+
+// function Section({ children, title }: SectionProps): JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[styles.sectionTitle, { color: isDarkMode ? Colors.white : Colors.black }]}
+//       >
+//         {title}
+//       </Text>
+//       <Text
+//         style={[styles.sectionDescription, { color: isDarkMode ? Colors.light : Colors.dark }]}
+//       >
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// }
+
+// function App(): JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   useEffect(() => {
+//     const myStuff = ['bag', 'shoes'];
+//     const props = {
+//       Name: 'Hello',
+//       Identity: '218',
+//       Email: 'jack@gmail.com',
+//       Phone: '+14155551234',
+//       Gender: 'M',
+//       DOB: new Date('1992-12-22T06:35:31'),
+//       'MSG-email': false,
+//       'MSG-push': true,
+//       'MSG-sms': false,
+//       'MSG-whatsapp': true,
+//       Stuff: myStuff
+//     };
+
+//     CleverTap.onUserLogin(props);
+//   }, []);
+//   CleverTap.createNotificationChannel("CtRNS", "Clever Tap React Native Testing", "CT React Native Testing", 5, true) // The notification channel importance can have any value from 1 to 5. A higher value means a more interruptive notification.
+//   let localInApp = {
+//     inAppType: 'alert',
+//     titleText: 'Get Notified',
+//     messageText: 'Enable Notification permission',
+//     followDeviceOrientation: true,
+//     positiveBtnText: 'Allow',
+//     negativeBtnText: 'Cancel',
+//     fallbackToSettings: true, //Setting this parameter to true will open an in-App to redirect you to Mobile's OS settings page.
+//   };
+
+// CleverTap.promptPushPrimer(localInApp);
+// CleverTap.promptForPushPermission(true);
+
+
+
+// CleverTap.addListener(CleverTap.CleverTapPushPermissionResponseReceived, (e)=>{/*consume the event*/})
+// //CleverTap.removeListener(CleverTap.CleverTapPushPermissionResponseReceived);  
+
+
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar
+//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+//         backgroundColor={backgroundStyle.backgroundColor}
+//       />
+//       <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+//         <Header />
+//         <View style={{ backgroundColor: isDarkMode ? Colors.black : Colors.white }}>
+//           {/* <Section title="Step One">
+//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+//             screen and then come back to see your edits.
+//           </Section> */}
+//           {/* <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks /> */}
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+// export default App;
+// tp
+// anhdbadjadkbadb.kjabkabdkj.abdkjabdjkbad.kbackhbckhabchkakhcvahcah.cbakbkabj.n jakcbkacb a.cb 
+
+// import CleverTap from 'clevertap-react-native';
+// import React, { useEffect } from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
+
+// import {
+//   Colors,
+//   Header,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// function App(): JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   useEffect(() => {
+//     const myStuff = ['bag', 'shoes'];
+//     const props = {
+//       Name: 'Hello',
+//       Identity: '218',
+//       Email: 'jack@gmail.com',
+//       Phone: '+14155551234',
+//       Gender: 'M',
+//       DOB: new Date('1992-12-22T06:35:31'),
+//       'MSG-email': false,
+//       'MSG-push': true,
+//       'MSG-sms': false,
+//       'MSG-whatsapp': true,
+//       Stuff: myStuff
+//     };
+
+//     CleverTap.onUserLogin(props);
+
+//     // // Add event listener for in-app notification button tap
+//     // CleverTap.addListener(CleverTap.CleverTapInAppNotificationButtonTapped, (event) => {
+//     //   _handleCleverTapEvent(CleverTap.CleverTapInAppNotificationButtonTapped, event);
+//     // });
+
+//     // Cleanup listener on component unmount
+//     return () => {
+//       CleverTap.removeListener(CleverTap.CleverTapInAppNotificationButtonTapped);
+//     };
+//   }, []);
+
+//   const _handleCleverTapEvent = (eventName: string, event: any) => {
+//     console.log('CleverTap Event called - ', eventName, event);
+//   };
+
+//   const handleButtonPress = () => {
+//     CleverTap.recordEvent("In-app 3 Notification", { "Product name": "CleverTap React Native" });
+//   };
+
+//   CleverTap.initializeInbox();
+
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar
+//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+//         backgroundColor={backgroundStyle.backgroundColor}
+//       />
+//       <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+//         <Header />
+//         <View style={{ backgroundColor: isDarkMode ? Colors.black : Colors.white }}>
+//           <TouchableOpacity
+//             style={styles.button}
+//             onPress={handleButtonPress}
+//           >
+//             <Text style={styles.buttonText}>In-app 3 Notification</Text>
+//           </TouchableOpacity>
+
+          
+
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   button: {
+//     backgroundColor: '#4CAF50',
+//     padding: 10,
+//     marginBottom: 10,
+//     alignItems: 'center',
+//     borderRadius: 5,
+//   },
+//   buttonText: {
+//     color: 'white',
+//     fontSize: 16,
+//     fontWeight: '600',
+//   },
+// });
+
+// export default App;
+import CleverTap from 'clevertap-react-native';
+import React, { useEffect } from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,34 +269,101 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  useEffect(() => {
+    const myStuff = ['bag', 'shoes'];
+    const props = {
+      Name: 'Hello',
+      Identity: '218',
+      Email: 'jack@gmail.com',
+      Phone: '+14155551234',
+      Gender: 'M',
+      DOB: new Date('1992-12-22T06:35:31'),
+      'MSG-email': false,
+      'MSG-push': true,
+      'MSG-sms': false,
+      'MSG-whatsapp': true,
+      Stuff: myStuff,
+    };
+
+    CleverTap.onUserLogin(props);
+
+  }, []);
+  
+    // Initialize inbox once
+    CleverTap.initializeInbox();
+
+  // Add listener for inbox initialization
+  useEffect(() => {
+    const inboxListener = (event: any) => {
+      _handleCleverTapInbox(CleverTap.CleverTapInboxDidInitialize, event);
+    };
+
+    CleverTap.addListener(CleverTap.CleverTapInboxDidInitialize, inboxListener);
+
+    return () => {
+      CleverTap.removeListener(CleverTap.CleverTapInboxDidInitialize);
+    };
+  }, []);
+
+  // // Add listener for when inbox messages are updated
+  // useEffect(() => {
+  //   const inboxUpdateListener = (event: any) => {
+  //     _handleCleverTapInbox(CleverTap.CleverTapInboxMessagesDidUpdate, event);
+  //   };
+
+  //   CleverTap.addListener(CleverTap.CleverTapInboxMessagesDidUpdate, inboxUpdateListener);
+
+  //   return () => {
+  //     CleverTap.removeListener(CleverTap.CleverTapInboxMessagesDidUpdate);
+  //   };
+  // }, []);
+
+  const _handleCleverTapInbox = (eventName: string, event: any) => {
+    CleverTap.showInbox({
+      tabs: ['Offers', 'Promotions'],
+      navBarTitle: 'My App Inbox',
+      navBarTitleColor: '#FF0000',
+      navBarColor: '#FFFFFF',
+      inboxBackgroundColor: '#AED6F1',
+      backButtonColor: '#00FF00',
+      unselectedTabColor: '#0000FF',
+      selectedTabColor: '#FF0000',
+      selectedTabIndicatorColor: '#000000',
+      noMessageText: 'No message(s)',
+      noMessageTextColor: '#FF0000',
+    });
+  };
+
+  const handleButtonPress = () => {
+    CleverTap.recordEvent('In-app 3 Notification', { 'Product name': 'CleverTap React Native' });
+  };
+  
+  const handleButtonPress2 = () => {
+    CleverTap.recordEvent('inbox', { 'Inbox name': 'CleverTap React Native' });
+  };
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={{ backgroundColor: isDarkMode ? Colors.black : Colors.white }}>
+          <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
+            <Text style={styles.buttonText}>In-app 3 Notification</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              CleverTap.initializeInbox();
+              handleButtonPress2();
+            }}
+          >
+            <Text style={styles.buttonText}>Inbox</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -97,21 +371,17 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    marginBottom: 10,
+    alignItems: 'center',
+    borderRadius: 5,
   },
-  sectionTitle: {
-    fontSize: 24,
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
     fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
 
--- android/app/src/main/java/com/myproject/MainApplication.java
+++ android/app/src/main/java/com/myproject/MainApplication.java
@@ -1,5 +1,6 @@
 package com.myproject;
 
+import com.clevertap.react.CleverTapPackage;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -9,40 +10,47 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import java.util.Map; // Add this import for Map
+import java.util.HashMap; // Add this import for HashMap
+import com.clevertap.react.CleverTapApplication;
+import com.clevertap.android.sdk.ActivityLifecycleCallback;
+import com.clevertap.android.sdk.CleverTapAPI;
+import com.clevertap.android.sdk.CleverTapAPI.LogLevel;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends CleverTapApplication implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new DefaultReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+  private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
-        }
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for
+      // example:
+      // packages.add(new MyReactNativePackage());
+      // packages.add(new CleverTapPackage());// only needed when not auto-linking
+      return packages;
+    }
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
 
-        @Override
-        protected boolean isNewArchEnabled() {
-          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
+    @Override
+    protected boolean isNewArchEnabled() {
+      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
 
-        @Override
-        protected Boolean isHermesEnabled() {
-          return BuildConfig.IS_HERMES_ENABLED;
-        }
-      };
+    @Override
+    protected Boolean isHermesEnabled() {
+      return BuildConfig.IS_HERMES_ENABLED;
+    }
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -51,10 +59,14 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public void onCreate() {
+    ActivityLifecycleCallback.register(this);
     super.onCreate();
+    CleverTapAPI.setDebugLevel(LogLevel.VERBOSE);
     SoLoader.init(this, /* native exopackage */ false);
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      // If you opted-in for the New Architecture, we load the native entry point for
+      // this app.
       DefaultNewArchitectureEntryPoint.load();
     }
     ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
